64f2d5cb1b02ad538f3e2f34ecd0a8de5d51c248
Dim <dmitry.byzin@yandex.ru>
2020-11-17 00:30:51 +0700

    register-data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./Fontawesomeicons";
+import "./App.css";
+import UsersList from "./UsersList";
+import RegisterForm from "./RegisterForm";
+import Login from "./Login";
+import Homepage from "./Homepage";
+
+import { FormProvider } from "./FormContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FormProvider>
+      <div>
+        <Router>
+          <Route path="/" exact component={RegisterForm} />
+          <Route path="/login" component={Login} />
+          <Route path="/homepage" component={Homepage} />
+          <Route path="/details" exact component={UsersList} />
+        </Router>
+      </div>
+    </FormProvider>
   );
 }
 
