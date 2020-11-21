6468d9608cd31e506a864411cb38ae78890bd0d6
Dim <dmitry.byzin@yandex.ru>
2020-11-22 00:50:13 +0700

    update

--- src/RegisterForm.js
+++ src/RegisterForm.js
@@ -5,7 +5,7 @@ import { useHistory } from "react-router-dom";
 
 const RegisterForm = () => {
   const history = useHistory();
-  const { register, handleSubmit, errors } = useForm({});
+  const { register, handleSubmit, errors, setError } = useForm({});
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
@@ -45,7 +45,7 @@ const RegisterForm = () => {
 
   return (
     <div className="wrapper">
-      <div className="form-wrapper">
+      <form className="form-wrapper" onSubmit={handleSubmit(onSubmitForm)}>
         <h1>
           Регистрация
         </h1>
@@ -56,82 +56,85 @@ const RegisterForm = () => {
           </Link>
         </small>
 
-        <form className="register" onSubmit={handleSubmit(onSubmitForm)}>
-          <div className="firstName">
-            <label htmlFor="firstName">Имя</label>
-            <input
-              className=""
-              placeholder="Введите Ваше имя"
-              type="text"
-              value={name}
-              name="name"
-              ref={register({
-                required: "Введено некорректное значение",
-                pattern: {
-                  value: /^[а-яА-ЯёЁa-zA-Z][а-яА-ЯёЁa-zA-Z- \.]{1,20}$/,
-                  message: "Введите действительное имя",
-                },
-              })}
-              onChange={updateName}
-            />
-            {errors.name && (
-              <p className="error">
-                <span>{errors.name.message}</span>
-              </p>
-            )}
-          </div>
+        <div className="firstName">
+          <label htmlFor="firstName">Имя</label>
+          <input
+            className=""
+            placeholder="Введите Ваше имя"
+            type="text"
+            value={name}
+            name="name"
+            ref={register({
+              required: "Введено некорректное значение",
+              pattern: {
+                value: /^[а-яА-ЯёЁa-zA-Z][а-яА-ЯёЁa-zA-Z- \.]{1,20}$/,
+                message: "Введите действительное имя",
+              },
+            })}
+            onChange={updateName}
+          />
 
-          <div className="email">
-            <label htmlFor="email">Email</label>
-            <input
-              className=""
-              placeholder="Введите Ваш Email"
-              type="email"
-              name="email"
-              value={email}
-              ref={register({
-                required: "Введено некорректное значение",
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                  message: "Введите действительный адрес электронной почты",
-                },
-              })}
-              onChange={updateEmail}
-            />
-            {errors.email && (
-              <p className="error">
-                <span>{errors.email.message}</span>
-              </p>
-            )}
-          </div>
+        </div>
+        <div className="name_error">
+          {errors.name && (<span>
+            {errors.name.message}</span>
+          )}
+        </div>
 
-          <div className="phone">
-            <label htmlFor="phone">Номер телефона</label>
-            <input
-              className=" "
-              placeholder="Введите номер"
-              type="text"
-              value={phone}
-              name="phone"
-              ref={register({
-                required: "Введено некорректное значение",
-                pattern: {
-                  value: /^[0-9|\ |\-|\(|\)|\+]{10,17}[0-9|\ |\-|\(|\)]$/,
-                  message: "Введите действительный номер телефона",
-                },
-              })}
-              onChange={updatePhone}
-            />
-            {errors.phone && (
-              <p className="error">
-                <span>{errors.phone.message}</span>
-              </p>
-            )}
-          </div>
+        <div className="email">
+          <label htmlFor="email">Email</label>
+          <input
+            className=""
+            placeholder="Введите Ваш Email"
+            type="email"
+            name="email"
+            value={email}
+            ref={register({
+              required: "Введено некорректное значение",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                message: "Введите действительный адрес электронной почты",
+              },
+            })}
+            onChange={updateEmail}
+          />
 
-          <div className="language">
-            <label htmlFor="language">Язык</label>
-            <select
+        </div>
+        <div className="name_error">
+          {errors.email && (<span>
+            {errors.email.message}</span>
+          )}
+        </div>
+
+        <div className="phone">
+          <label htmlFor="phone">Номер телефона</label>
+          <input
+            className=" "
+            placeholder="Введите номер телефона"
+            type="text"
+            value={phone}
+            name="phone"
+            ref={register({
+              required: "Введено некорректное значение",
+              pattern: {
+                value: /^[0-9|\ |\-|\(|\)|\+]{10}[0-9|\ |\-|\(|\)]$/,
+                message: "Введите действительный номер телефона",
+              },
+            })}
+            onChange={updatePhone}
+          />
+        </div>
+
+        <div className="name_error">
+          {errors.phone && (<span>
+            {errors.phone.message}</span>
+          )}
+        </div>
+
+        <div className="language">
+          <label htmlFor="language">Язык</label>
+          <div>
+            <select required
               className="custom-select"
               name="language"
               value={language}
@@ -141,57 +144,63 @@ const RegisterForm = () => {
               })}
               onChange={updateLanguage}
             >
-              <option></option>
+              <option value="" hidden style={{ color: "#7C9CBF" }}>Язык</option>
               <option>Русский</option>
               <option>Английский</option>
               <option>Китайский</option>
               <option>Испанский</option>
             </select>
-            {errors.language && (
-              <p className="error">
-                <span>{errors.language.message}</span>
-              </p>
-            )}
           </div>
+        </div>
 
-          <div className="checkbox">
-            <label for="check"></label>
-            <input id="check"
-              type="checkbox"
-              value="check"
-              name="tnc"
-              value={tnc}
-              ref={register({
-                required: "Введено некорректное значение",
-              })}
-              onChange={updateTnc}
-            />
-            <span className="accept_use">Принимаю <a href="#" className="conditions">условия</a> использования</span>
-            {errors.tnc && (
-              <p className="error">
-                <span>{errors.tnc.message}</span>
-              </p>
-            )}
+        <div className="name_error">
+          {errors.language && (<span>
+            {errors.language.message}</span>
+          )}
+        </div>
 
-          </div>
+        <div className="checkbox">
+          <label for="check"></label>
+          <input
+            id="check"
+            type="checkbox"
+            value="check"
+            name="tnc"
+            value={tnc}
+            ref={register({
+              required: "Введено некорректное значение",
+            })}
+            onChange={updateTnc}
+          />
+          <span className="accept_use">Принимаю <a href="#" className="conditions">условия</a> использования</span>
+        </div>
 
-          <div className="createAccount">
-            <Link to="/login" style={{ width: "100%" }}>
-              <button type="submit" onClick={handleSubmit(onSubmit)}>
-                Зарегистрироваться
-              </button>
-            </Link>
-            {errors.name &&
-              errors.email &&
-              errors.phone &&
-              errors.language &&
-              errors.tnc &&
-              "Все поля обязательны для заполнения"}
-          </div>
-        </form>
-      </div>
+        <div className="name_error">
+          {errors.tnc && (<span>
+            {errors.tnc.message}</span>
+          )}
+        </div>
+
+        <div className="createAccount">
+          <Link to="/login" style={{ width: "100%" }}>
+            <button type="submit" onClick={handleSubmit(onSubmit)}>
+              Зарегистрироваться
+            </button>
+          </Link>
+        </div>
+
+        <div className="name_error">
+          {
+            errors.name &&
+            errors.email &&
+            errors.phone &&
+            errors.language &&
+            errors.tnc &&
+            <div className="all_error">Все поля обязательны для заполнения</div>
+          }
+        </div>
+      </form>
     </div>
-
   );
 };
 export default RegisterForm;
